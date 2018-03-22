cde3b67d8eda1a9925774fd5d4830f009ac7c107
ericdasse28 <christ.gououi@gmail.com>
2018-03-22 17:45:53 +0100

    Provide raw visualization for better data understanding

---
+++ Landslides_analysis.R
@@ -0,0 +1,9 @@
+# Loading the dataset
+landslides <- read.csv("Data/Global_Landslide_Catalog_Export.csv", header = TRUE)
+
+######################
+# Data Understanding #
+######################
+summary(landslides)
+View(landslides)
+
