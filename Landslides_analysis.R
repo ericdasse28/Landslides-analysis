bc141e263c6df64c2c8adc64f3efa179eb7f87ff
ericdasse28 <christ.gououi@gmail.com>
2018-03-22 18:27:19 +0100

    Add Data Preparation part to the project. Ignore the columns with too much missing informations

--- Landslides_analysis.R
+++ Landslides_analysis.R
@@ -7,3 +7,18 @@ landslides <- read.csv("Data/Global_Landslide_Catalog_Export.csv", header = TRUE
 summary(landslides)
 View(landslides)
 
+
+
+####################
+# Data Preparation #
+####################
+
+# Ignoring the time column (because it lacks too much information)
+landslides$time <- NULL
+summary(landslides)
+
+
+# Same for storm_name, local_description, photos_link
+landslides$storm_name <- NULL
+landslides$local_description <- NULL
+landslides$photos_link <- NULL
