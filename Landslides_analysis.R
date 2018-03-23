307f7be6ae092a96f8fe59c44c9d50b713324e5f
ericdasse28 <christ.gououi@gmail.com>
2018-03-23 09:39:02 +0100

    Start visualization

--- Landslides_analysis.R
+++ Landslides_analysis.R
@@ -27,7 +27,7 @@ summary(landslides)
 
 
 # Now, let's delete the irrelevant columns (hazard_type, source_name, source_link, cat_src,
-# cat_id, adminname1, adminname2, changeset_id, version)
+# cat_id, adminname1, adminname2, changeset_id, version, continentcode)
 landslides$hazard_type <- NULL
 landslides$source_name <- NULL
 landslides$source_link <- NULL
@@ -37,5 +37,37 @@ landslides$adminname1 <- NULL
 landslides$adminname2 <- NULL
 landslides$changeset_id <- NULL
 landslides$version <- NULL
+landslides$continentcode <- NULL
 
 summary(landslides)
+
+
+# There are several rows with missing information, starting the ones in the "fatalities" and
+# "injuries" columns
+landslides <- subset(landslides, !is.na(landslides$fatalities))
+landslides <- subset(landslides, !is.na(landslides$injuries))
+
+summary(landslides)
+
+# Remove the missing values for population and distance
+landslides <- subset(landslides, !is.na(landslides$population))
+landslides <- subset(landslides, !is.na(landslides$distance))
+
+summary(landslides)
+
+
+# Renaming values for more convenience
+landslides$landslide_type <- ifelse(landslides$landslide_type %in%
+                                      c('land'))
+
+
+##################
+# VISUA+LIZATION #
+##################
+
+# Plot of number of number landslides cases per city
+library(ggplot2)
+
+
+# Plot with respect to the types of landslides
+qplot(landslides$)
